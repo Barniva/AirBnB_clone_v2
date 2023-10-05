2fbaefc54d185ed6dd0edd6f4da7d881aa1c61b1
Barniva <barnabasva1@gmail.com>
2023-10-05 10:32:12 +0300

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,12 @@
 #!/usr/bin/env bash
-# a script that make directory and Install Nginx if not already installed
-if ! dpkg -l | grep -q nginx; then
-    apt-get update
-    apt-get install -y nginx
-fi
+# Write a Bash script that sets up your web servers for the deployment of web_static. It must:
 
-folders=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/shared/" "/data/web_static/releases/test/")
-for folder in "${folders[@]}"; do
-    if [ ! -d "$folder" ]; then
-        mkdir -p "$folder"
-    fi
-done
-
-fake_html="/data/web_static/releases/test/index.html"
-echo "Fake Content" > "$fake_html"
-
-symlink="/data/web_static/current"
-if [ -L "$symlink" ]; then
-    rm "$symlink"
-fi
-ln -s /data/web_static/releases/test/ "$symlink"
-
-chown -R ubuntu:ubuntu /data/
-
-nginx_config="/etc/nginx/sites-available/default"
-sed -i '/^[\s]*location \/ {$/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' "$nginx_config"
-
-service nginx restart
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo echo "<html><head></head><body>My web page</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
